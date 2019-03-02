5c21ff4db89b0d36c184d227dc0275e11bb12ea4
Danny Torres <danny.torresxd@gmail.com>
2019-03-02 13:11:16 -0500

    Adding swagger to the API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,11 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ * @SWG\Info(title="Merqueo - Technical Test", version="1.0.0")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
